610190cd6ae734a6842fc193770b4ea7ee6bc5ce
Ashok Menon <ashokmenon@fb.com>
2018-04-22 20:00:34 +0100

    Migrate to React project
    (Nothing works anymore)

---
+++ src/Options.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Options.css';
+
+class Options extends Component {
+  render() {
+    return (
+      <div className="Options">
+        Options!
+      </div>
+    );
+  }
+}
+
+export default Options;
